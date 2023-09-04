ee6d575d4983eb21bfb2a0b1d094623e392e720f
sam <shop2local@gmail.com>
2023-09-04 10:04:44 +0000

    service worker

---
+++ sw.js
@@ -0,0 +1,27 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('num2words-cache-v1').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css',
+          'https://code.jquery.com/jquery-3.5.1.slim.min.js',
+          '/img/n2w.png',
+          '/img/n2w-192.png',
+          '/img/n2w-512.png',
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+    );
+  });
+  
