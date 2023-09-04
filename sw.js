0db119007850de5eb6feb1e7e068abda30644d6c
sam <shop2local@gmail.com>
2023-09-04 11:06:14 +0000

    updated paths

--- sw.js
+++ sw.js
@@ -2,13 +2,13 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
       caches.open('num2words-cache-v1').then(function(cache) {
         return cache.addAll([
-          '/',
-          '/index.html',
+          '',
+          'index.html',
           'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css',
           'https://code.jquery.com/jquery-3.5.1.slim.min.js',
-          '/img/n2w.png',
-          '/img/n2w-192.png',
-          '/img/n2w-512.png',
+          'img/n2w.png',
+          'img/n2w-192.png',
+          'img/n2w-512.png',
         ]);
       })
     );
